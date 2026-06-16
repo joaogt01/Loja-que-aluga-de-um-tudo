6a8de74bdfbbdd274b55852c586520b84e018122
Arthur Dias <arthurdiasm28@gmail.com>
2026-06-15 21:44:28 -0300

    Implementando o business Fornecedor, seus métodos e construtor. Os métodos do business são para formar o CRUD da classe, que incluem: cadastrar, atualizar, listar e desativar. Ainda não foi implementado o método listar.

---
+++ Loja-que-aluga-de-um-tudo/loja-aluga-tudo/src/main/java/com/loja/business/FornecedorBusiness.java
@@ -0,0 +1,39 @@
+package com.loja.business;
+
+import com.loja.model.Fornecedor;
+import com.loja.repositories.FornecedorRepository;
+
+public class FornecedorBusiness {
+    private FornecedorRepository fornecedorRepository;
+
+    public FornecedorBusiness(FornecedorRepository fornecedorRepository) {
+        this.fornecedorRepository = fornecedorRepository;
+    }
+
+    public void cadastrar(Fornecedor fornecedor) {
+        if (fornecedorRepository.buscarPorCnpj(fornecedor.getCnpj()) != null) {
+            throw new RuntimeException("CNPJ já cadastrado: " + fornecedor.getCnpj());
+        }
+        fornecedorRepository.salvar(fornecedor);
+    }
+
+    public void atualizar(String id, Fornecedor dados) {
+        Fornecedor existente = fornecedorRepository.buscarPorId(id);
+        if (existente == null) {
+            throw new RuntimeException("Fornecedor não encontrado: " + id);
+        }
+        existente.setNome(dados.getNome());
+        existente.setCnpj(dados.getCnpj());
+        existente.setTelefone(dados.getTelefone());
+        fornecedorRepository.salvar(existente);
+    }
+
+    public void desativar(String id) {
+        Fornecedor existente = fornecedorRepository.buscarPorId(id);
+        if (existente == null) {
+            throw new RuntimeException("Fornecedor não encontrado: " + id);
+        }
+        existente.setAtivo(false);
+        fornecedorRepository.salvar(existente);
+    }
+}
