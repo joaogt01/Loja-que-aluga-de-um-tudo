ff6ee1cf057f7c9283a2f61d1058fc41266d5d93
Renan Alves <renan.alima99@gmail.com>
2026-06-23 13:48:56 -0300

    feat: implementa ContratoRepository

--- Loja-que-aluga-de-um-tudo/loja-aluga-tudo/src/main/java/com/loja/repositories/interfaces/ContratoRepository.java
+++ Loja-que-aluga-de-um-tudo/loja-aluga-tudo/src/main/java/com/loja/repositories/interfaces/ContratoRepository.java
@@ -1,4 +1,21 @@
 package com.loja.repositories.interfaces;
 
+import com.loja.model.ContratoAluguel;
+
+import java.util.List;
+import java.util.Map;
+
 public interface ContratoRepository {
+    void salvar(ContratoAluguel contrato);
+    ContratoAluguel buscar(String id);
+
+    Map<String, ContratoAluguel> listar();
+    Map<String, ContratoAluguel> listarPorCliente(String clienteId);
+    Map<String, ContratoAluguel> listarPorStatus(String status);
+
+    void atualizar(String id);
+    void deletar(String id);
+
+    void carregarDados();
+    void salvarDados();
 }
